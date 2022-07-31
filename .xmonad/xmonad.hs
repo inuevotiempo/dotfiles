f249e892c069872314b79c5bb80431df8b9f4f4d
Dilan Grajales <dilangrajales1128@gmail.com>
2022-07-31 00:08:00 -0500

    Cambio de configuracion Julio 31, 2022

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -27,7 +27,6 @@ import qualified Data.Map as M
     -- Hooks
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
 import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
-import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat, doCenterFloat)
 import XMonad.Hooks.ServerMode
